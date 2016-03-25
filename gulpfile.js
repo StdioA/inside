7d4612c51d134cb61d17ab5506bb13a8b8969e68
Stdio A <stdio_dai@nuaa.edu.cn>
2016-03-25 10:55:11 +0800

    Using less & change the style of archive page
    And change the indentation specs

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp  = require('gulp');
 var uglify = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
 var rename = require('gulp-rename');
+var less = require('gulp-less')
 
 gulp.task("compress-js", [], function () {
 	return gulp.src('./mblog/static/mblog/js/!(*.min).js')
@@ -10,7 +11,13 @@ gulp.task("compress-js", [], function () {
 			.pipe(gulp.dest('./mblog/static/mblog/js/'))
 });
 
-gulp.task("compress-css", [], function () {
+gulp.task("less", [], function () {
+	return gulp.src('./mblog/static/mblog/less/*.less')
+			.pipe(less())
+			.pipe(gulp.dest('./mblog/static/mblog/css/'))
+})
+
+gulp.task("compress-css", ["less"], function () {
 	return gulp.src('./mblog/static/mblog/css/!(*.min).css')
 			.pipe(uglifycss())
 			.pipe(rename({ suffix: ".min" }))
@@ -19,7 +26,7 @@ gulp.task("compress-css", [], function () {
 
 gulp.task('watch', ["compress-js", "compress-css"], function () {
 	var watcher_js = gulp.watch('./mblog/static/mblog/js/*.js', ['compress-js']);
-	var watcher_css = gulp.watch('./mblog/static/mblog/css/*.css', ['compress-css']);
+	var watcher_css = gulp.watch('./mblog/static/mblog/less/*.less', ['less', 'compress-css']);
 	
 	watcher_js.on('change', function (event) {
 		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
