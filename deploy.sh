e44d2533ff6e8894bd2cc3b537f53da22430e806
Stdio A <stdio_dai@nuaa.edu.cn>
2016-03-30 20:56:06 +0800

    Edit dockerfile

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 python manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'myemail@example.com', 'password')" | python manage.py shell
-python manage.py runserver
+# python manage.py runserver
