75c909ffa688ce511f8bb22598a6797ddd805a7f
David Dai <yuchen.dai@shanbay.com>
2019-03-29 16:01:58 +0800

    Finish unittest for app backstage & mblog

---
+++ backstage/tests.py
@@ -0,0 +1,74 @@
+import json
+from io import BytesIO
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from mblog.models import Post, Comment
+
+
+class BackStageTest(TestCase):
+    def setUp(self):
+        self.posts = []
+        for i in range(1, 10):
+            post = Post.objects.create(content="Content {}".format(i))
+            self.posts.append(post)
+            for j in (1, 0):
+                cid = i * 2 - j
+                Comment.objects.create(
+                    post=post, author="author_{}".format(cid),
+                    content="Comment {}".format(cid))
+        self.client = Client()
+        self.user = User.objects.create(username="user", is_superuser=True)
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Post.objects.all().delete()
+        Comment.objects.all().delete()
+
+    def test_data_view(self):
+        """
+        Test accessbility for back-end
+        """
+        backstage_url = '/manage/data/'
+        res = self.client.get(backstage_url)
+        self.assertEqual(res.status_code, 404)
+
+        self.client.force_login(self.user)
+        res = self.client.get(backstage_url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_post_import_export(self):
+        self.client.force_login(self.user)
+
+        # Export data
+        export_url = '/manage/data/export/'
+        res = self.client.get(export_url)
+        self.assertEqual(res.status_code, 200)
+        exported_data = list(res.streaming_content)[0]
+        data = json.loads(exported_data)
+        self.assertEqual(len(data["posts"]), len(self.posts))
+        for post_obj, post_payload in zip(self.posts, data["posts"]):
+            for field in ("id", "exist", "content"):
+                self.assertEqual(getattr(post_obj, field), post_payload[field])
+
+        # Import data
+        import_url = '/manage/data/import/'
+        # Error: no data file
+        res = self.client.post(import_url)
+        self.assertEqual(res.status_code, 200)
+        self.assertFalse(res.context["success"])
+        self.assertEqual(res.context["message"], "Please Select data file.")
+        # Error: wrong file format
+        file = BytesIO(b'blahblahblah')
+        res = self.client.post(import_url, {"data": file})
+        self.assertFalse(res.context["success"])
+        self.assertEqual(res.context["message"], "JSON parsing error")
+
+        # Normal condition
+        file = BytesIO(exported_data)
+        res = self.client.post(import_url, {"data": file})
+        self.assertTrue(res.context["success"])
+        self.assertEqual(res.context["message"],
+                         "%d posts has been imported!" % len(self.posts))
+
+        self.assertEqual(Post.objects.count(), 2 * len(self.posts))
+        self.assertEqual(Comment.objects.count(), 2 * 2 * len(self.posts))
---
+++ mblog/tests.py
@@ -0,0 +1,111 @@
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from mblog.models import Post
+
+
+class MBlogAppTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_superuser(
+            "admin", "email", "password")
+        self.user2 = User.objects.create_user(
+            "user", "email", "password")
+        self.client = Client()
+
+    def tearDown(self):
+        for model in (User, Post):
+            model.objects.all().delete()
+
+    def login(self, user=None):
+        user = user or self.user
+        self.client.force_login(user)
+
+    def test_login_logout(self):
+        next_url = "/some_next"
+        login_url = "/login/?next={}".format(next_url)
+        logout_url = "/logout/"
+
+        res = self.client.get(login_url)
+        self.assertEqual(res.status_code, 200)
+
+        # Login failed
+        res = self.client.post(login_url, {
+            "username": self.user.username,
+            "password": "wrong"
+        })
+        self.assertIn("Invalid", res.context["error"])
+
+        # Login successfully
+        res = self.client.post(login_url, {
+            "username": self.user.username,
+            "password": "password"
+        })
+        self.assertEqual(res.status_code, 302)
+        self.assertEqual(res.url, next_url)
+        self.assertEqual(self.client.session["_auth_user_id"],
+                         str(self.user.id))
+
+        # Logout
+        res = self.client.get(logout_url)
+        self.assertEqual(res.status_code, 302)
+        self.assertEqual(res.url, "/")
+        self.assertNotIn("_auth_user_id", self.client.session)
+
+    def test_archive_page(self):
+        """
+        Test accessibility
+        """
+        url = "/archive/"
+        # Not login
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 302)
+        self.assertIn("login", res.url)
+
+        # Login
+        self.login()
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_index_page(self):
+        url = "/"
+        self.login()
+        # Returns 404 if no post exists
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 404)
+
+        # Redirect to latest post page
+        for i in range(1, 5):
+            post = Post.objects.create(content="Content {}".format(i))
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 302)
+        self.assertEqual(res.url, "/{}".format(post.id))
+
+    def test_create_post(self):
+        url = "/0"
+        self.login()
+
+        # Get view
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+        # Create post
+        content = "Test content"
+        res = self.client.post(url, {"content": content})
+        self.assertEqual(res.status_code, 302)
+        post = Post.objects.last()
+        self.assertEqual(post.content, content)
+        self.assertEqual(res.url, "/{}".format(post.id))
+
+    def test_view_post(self):
+        post = Post.objects.create(content="hhhhhh")
+        url = "/{}".format(post.id)
+        # Normal user will visit a static page
+        self.login(self.user2)
+        res = self.client.get(url)
+        self.assertIsNone(res.context)
+
+        # Admin will visit a dynamic page
+        self.login(self.user)
+        res = self.client.get(url)
+        self.assertEqual(res.context["post"], post)
+        self.assertEqual(res.context["previous"], 0)    # no post before
+        self.assertEqual(res.context["next"], 0)
